9e3ee4a085510283a1490b0511b281c360f37ac4
Ilshat VM <ilshatvm13@gmail.com>
2024-07-22 10:44:22 +0500

    lesson-07

--- app/src/main/java/com/example/telegram/ui/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/BaseFragment.kt
@@ -3,24 +3,32 @@ package com.example.telegram.ui.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import com.example.telegram.R
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 
-open class BaseFragment(private val layout:Int) : Fragment() {
+open class BaseFragment(private val layout: Int) : Fragment(), MenuProvider {
 
-    private lateinit var  mRootView: View
+    private lateinit var mRootView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
-        mRootView = inflater.inflate(layout,container,false)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        mRootView = inflater.inflate(layout, container, false)
         return mRootView
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {}
 
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return false
     }
 }
