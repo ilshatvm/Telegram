1589d371efdcdfb23285e715c430103a95f816dc
Ilshat VM <ilshatvm13@gmail.com>
2024-07-24 11:26:57 +0500

    lesson-08

---
+++ app/src/main/java/com/example/telegram/ui/activities/RegisterActivity.kt
@@ -0,0 +1,42 @@
+package com.example.telegram.ui.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.telegram.R
+import com.example.telegram.databinding.ActivityRegisterBinding
+import com.example.telegram.ui.fragments.EnterPhoneNumberFragment
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityRegisterBinding
+    private lateinit var mToolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityRegisterBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mToolbar = mBinding.registerToolbar
+        setSupportActionBar(findViewById(R.id.registerToolbar))
+        setTitle(R.string.register_title_your_phone)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.registerDataContainer, EnterPhoneNumberFragment())
+            .commit()
+    }
+}
